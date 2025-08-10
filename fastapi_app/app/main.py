ca2094f42242454de95f4043fd5f5344ca51c8c7
eunjaegu <eunjaegu@users.noreply.github.com>
2025-08-11 02:06:36 +0900

    update deployment settings

--- fastapi_app/app/main.py
+++ fastapi_app/app/main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from app.api.v1.routers import api_router
 from app.common.scheduler import register_periodic_tasks
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/fastapi",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+)
 app.include_router(api_router)
 
 # register_periodic_tasks(app)
