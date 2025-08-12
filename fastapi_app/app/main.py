861a0d46306a545b24c6d183a6f53e8ac96f2970
eunjaegu <eunjaegu@users.noreply.github.com>
2025-08-12 19:34:13 +0900

    Add environment-based config for production and development

--- fastapi_app/app/main.py
+++ fastapi_app/app/main.py
@@ -1,13 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from app.api.v1.routers import api_router
 from app.common.scheduler import register_periodic_tasks
 
-# nano /etc/environment 혹은 vi /etc/environment 접속
-# ENV=production 추가 작성 및 저장 후 종료
-# source /etc/environment 혹은 sudo reboot 재부팅(권장)
+load_dotenv("/app/envs/.prod.env")
+
 if os.getenv("ENV") == "production":
     app = FastAPI(
         root_path="/fastapi",
